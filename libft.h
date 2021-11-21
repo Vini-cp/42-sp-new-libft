6775c603c60b6552c96abf31dac078e5f2b720fd
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-11-21 19:53:34 -0300

    Add printf files

--- libft.h
+++ libft.h
@@ -4,8 +4,25 @@
 # include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdarg.h>
+# include <stdint.h>
 
 #define BUFFER_SIZE 32
+
+typedef struct s_flags
+{
+	char			*f;
+	char			*str;
+	char			*spaces;
+	char			print_c;
+	int				s_len;
+	int				just;
+	int				prec;
+	int				width;
+	int				zero;
+	int				neg;
+	char			c;
+}					t_flags;
 
 void				*ft_calloc(size_t nelem, size_t elsize);
 void				ft_bzero(void *s, size_t n);
@@ -38,9 +55,9 @@ char				**ft_split(char const *s, char c);
 char				*ft_strchr(const char *s, int c);
 char				*ft_strdup(const char *s);
 char				*ft_strjoin(char const *s1, char const *s2);
-size_t				ft_strlcat(char *dst, const char *src, size_t size);
-size_t				ft_strlcpy(char *dst, const char *src, size_t size);
-size_t				ft_strlen(const char *s);
+size_t			ft_strlcat(char *dst, const char *src, size_t size);
+size_t			ft_strlcpy(char *dst, const char *src, size_t size);
+size_t			ft_strlen(const char *s);
 char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 char				*ft_strnstr(const char *big, const char *little, size_t len);
@@ -49,5 +66,19 @@ char				*ft_strtrim(char const *s1, char const *set);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 void				ft_strrev(char *str);
 int					get_next_line(int fd, char **line);
+/* printf files */
+char				*ft_utoa(size_t num, int base, int capitalize);
+char				*ft_ptoa(uintptr_t num, int prec);
+char				*ft_ctoa(char c);
+int					ft_printf(const char *fmt, ...);
+int					ft_printf_putchar(char c);
+int					ft_printf_print(t_flags *lst);
+int					ft_printf_print_p_str(t_flags *lst);
+int					ft_printf_print_c(t_flags *lst);
+int					ft_printf_isflag(int c);
+void				ft_printf_empty_zeros(t_flags ***lst);
+void				ft_printf_get_flag(char *s, va_list args, t_flags ***lst);
+t_flags			*ft_printf_newflag(void);
+void				to_string(const char *f, va_list args, t_flags ***lst);
 
 #endif
---
+++ src/ft_printf_isflag.c
@@ -0,0 +1,9 @@
+#include "../libft.h"
+
+int	ft_printf_isflag(int c)
+{
+	if (c == '*' || c == '.' || c == '-')
+		return (1);
+	else
+		return (0);
+}
